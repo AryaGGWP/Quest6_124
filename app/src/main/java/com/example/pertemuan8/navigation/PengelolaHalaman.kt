8edc1d881893dadc1df92f8e9d798393469e798f
AryaGGWP <arya.lampung.2016@gmail.com>
2024-11-21 00:08:19 +0700

    22nd commit: menambahkan Pengelola Halaman

--- app/src/main/java/com/example/pertemuan8/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/pertemuan8/navigation/PengelolaHalaman.kt
@@ -1,2 +1,25 @@
 package com.example.pertemuan8.navigation
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.pertemuan8.ui.view.viewmodel.MahasiswaViewModel
+import com.example.pertemuan8.ui.view.viewmodel.RencanaStudiViewModel
+
+enum class Halaman{
+    Splash,
+    Mahasiswa,
+    Matakuliah,
+    Tampil
+}
+
+@Composable
+fun MahasiswaApp(
+    modifier: Modifier = Modifier,
+    mahasiswaViewModel: MahasiswaViewModel = viewModel(),
+    krsViewModel: RencanaStudiViewModel = viewModel(),
+    navController: NavHostController = rememberController()
+){
+    val mahasiswaUiState = mahasiswaViewModel.mahasiswaUiState.collectAsState().value
+    NavHost
+
+}
